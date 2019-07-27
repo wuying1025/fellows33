4fe8dac150faaf41967f99ed19e5a1d20d0f9389
wuying1025 <wuy_123@126.com>
2019-07-27 16:09:54 +0800

    hdfs

---
+++ mapreducer/src/wordcount/WordCountMapper.java
@@ -0,0 +1,20 @@
+package wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WordCountMapper extends Mapper<LongWritable,Text,Text,IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        // 分割字符串
+        String[] words = line.split(" ");
+        for (String word: words) {
+            context.write(new Text(word),new IntWritable(1));
+        }
+    }
+}
