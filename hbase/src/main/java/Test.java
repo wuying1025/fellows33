73dfc2b5a4fb2d914118578b4d6ac0622c31c7e7
wuying1025 <wuy_123@126.com>
2019-08-13 15:11:48 +0800

    hbase

--- hbase/src/main/java/Test.java
+++ hbase/src/main/java/Test.java
@@ -1,20 +1,18 @@
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Admin;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.ConnectionFactory;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.*;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
     static private Admin admin = null;
     static private Connection connection = null;
+    static private Configuration conf = null;
     static {
-        Configuration conf =  HBaseConfiguration.create();
+        conf =  HBaseConfiguration.create();
         conf.set("hbase.zookeeper.quorum", "192.168.1.100");
         try {
             connection = ConnectionFactory.createConnection(conf);
@@ -34,6 +32,9 @@ public class Test {
     }
 
     public static void createTable(String tableName,String... fc) throws Exception {
+        if(isExist(tableName)){
+            return;
+        }
         TableName table =TableName.valueOf(tableName);
         HTableDescriptor hTableDescriptor = new HTableDescriptor(table);
         for (String s : fc) {
@@ -41,11 +42,84 @@ public class Test {
         }
         admin.createTable(hTableDescriptor);
     }
+    public static void deleteTable(String tableName) throws Exception {
+        if(!isExist(tableName)){
+           return;
+        }
+        admin.disableTable(TableName.valueOf(tableName));
+        admin.deleteTable(TableName.valueOf(tableName));
+    }
+
+    public static void insertData(String tableName,String rowkey,String cf,String name,String value) throws IOException {
+        HTable table = new HTable(conf,tableName);
+        Put put = new Put(Bytes.toBytes(rowkey));
+        put.addColumn(Bytes.toBytes(cf),Bytes.toBytes(name),Bytes.toBytes(value));
+        table.put(put);
+    }
+
+    public static void delData(String tableName,String... rowkey) throws IOException {
+        HTable table = new HTable(conf,tableName);
+        List<Delete> ld = new ArrayList<Delete>();
+        for (String s : rowkey) {
+            Delete del = new Delete(Bytes.toBytes(s));
+            ld.add(del);
+        }
+        table.delete(ld);
+    }
+    public static void get(String tableName) throws IOException {
+        HTable table = new HTable(conf,tableName);
+        Scan sc = new Scan();
+        ResultScanner scanner = table.getScanner(sc);
+        for (Result result : scanner) {
+            Cell[] cells = result.rawCells();
+            for (Cell cell : cells) {
+                System.out.println(Bytes.toString(CellUtil.cloneRow(cell))+",");
+                System.out.println(Bytes.toString(CellUtil.cloneFamily(cell))+",");
+                System.out.println(Bytes.toString(CellUtil.cloneQualifier(cell))+",");
+                System.out.println(Bytes.toString(CellUtil.cloneValue(cell)));
+            }
+        }
+    }
+    public static void getRow(String tableName,String rowkey) throws IOException {
+        HTable table = new HTable(conf,tableName);
+        Get get = new Get(Bytes.toBytes(rowkey));
+        Result result = table.get(get);
+        Cell[] cells = result.rawCells();
+        for (Cell cell : cells) {
+            System.out.println(Bytes.toString(CellUtil.cloneRow(cell)) + ",");
+            System.out.println(Bytes.toString(CellUtil.cloneFamily(cell)) + ",");
+            System.out.println(Bytes.toString(CellUtil.cloneQualifier(cell)) + ",");
+            System.out.println(Bytes.toString(CellUtil.cloneValue(cell)));
+        }
+    }
+
+    public static void getRowcf(String tableName,String rowkey,String cf,String name) throws IOException {
+        HTable table = new HTable(conf,tableName);
+        Get get = new Get(Bytes.toBytes(rowkey));
+        get.addColumn(Bytes.toBytes(cf),Bytes.toBytes(name));
+        Result result = table.get(get);
+        Cell[] cells = result.rawCells();
+        for (Cell cell : cells) {
+            System.out.println(Bytes.toString(CellUtil.cloneRow(cell)) + ",");
+            System.out.println(Bytes.toString(CellUtil.cloneFamily(cell)) + ",");
+            System.out.println(Bytes.toString(CellUtil.cloneQualifier(cell)) + ",");
+            System.out.println(Bytes.toString(CellUtil.cloneValue(cell)));
+        }
+    }
+
 
     public static void main(String[] args) throws Exception {
-        System.out.println(isExist("student"));
-        System.out.println(isExist("student1"));
-        createTable("student1","info");
-        System.out.println(isExist("student1"));
+//        System.out.println(isExist("student"));
+//        System.out.println(isExist("student1"));
+//        createTable("student1","info");
+//        System.out.println(isExist("student1"));
+//        deleteTable("student1");
+//        insertData("student","1001","info","name","zs");
+//        insertData("student","1002","info","name","lisi");
+//        insertData("student","1001","info","age","13");
+//        delData("student","1001","1000");
+//        get("student");
+//        getRow("student","1001");
+        getRowcf("student","1001","info","age");
     }
 }
