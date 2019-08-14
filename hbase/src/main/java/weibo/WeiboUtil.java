ca30225165d1facefccd7042e5200e25f4e3b96a
wuying1025 <wuy_123@126.com>
2019-08-14 15:45:57 +0800

    hbase

--- hbase/src/main/java/weibo/WeiboUtil.java
+++ hbase/src/main/java/weibo/WeiboUtil.java
@@ -2,12 +2,12 @@ package weibo;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.*;
-import org.apache.hadoop.hbase.client.Admin;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.ConnectionFactory;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WeiboUtil {
     private static Configuration conf = null;
@@ -36,6 +36,32 @@ public class WeiboUtil {
             hTableDescriptor.addFamily(chd);
         }
         admin.createTable(hTableDescriptor);
+    }
+
+    public static void putContent(String uid,String content) throws Exception {
+        Table conTable = connection.getTable(TableName.valueOf(Constant.WEIBO_CONTENT));
+        Table relTable = connection.getTable(TableName.valueOf(Constant.WEIBO_RELATIONS));
+        Table recTable = connection.getTable(TableName.valueOf(Constant.WEIBO_RECEIVE_CONTENT_EMAIL));
+        //1.插入微博
+        long l = System.currentTimeMillis();
+        String rowKey = uid + "_" + l;
+        Put put = new Put(Bytes.toBytes(rowKey));
+        put.addColumn(Bytes.toBytes("info"),Bytes.toBytes("content"),Bytes.toBytes(content));
+        conTable.put(put);
+        //2.找用户的关注人
+        Get g = new Get(Bytes.toBytes(uid));
+        g.addFamily(Bytes.toBytes("fans"));
+        Result result = relTable.get(g);
+        Cell[] cells = result.rawCells();
+        List<Put> lp = new ArrayList<Put>();
+        for (Cell cell : cells) {
+            byte[] qualifier = CellUtil.cloneQualifier(cell);//粉丝 B
+            //3.加入微博收件箱表
+            Put p = new Put(qualifier);
+            p.addColumn(Bytes.toBytes("info"),Bytes.toBytes(uid),Bytes.toBytes(rowKey));
+            lp.add(p);
+        }
+        recTable.put(lp);
     }
 
     public static void main(String[] args) throws Exception {
